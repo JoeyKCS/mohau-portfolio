3ed97b040363c9b456d855a1d6edadbabc72ad9a
Mohau <106928210+JoeyKCS@users.noreply.github.com>
2024-06-09 20:57:43 +0200

    Add files via upload

---
+++ data/index.ts
@@ -0,0 +1,139 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Prioritizing Client Collaboration and Open Communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "Constantly trying to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast dedicated to innovative development solutions.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently working on a 3JS project ",
+      description: "Interactive 3JS website ",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Metaverse",
+      des: "Modern Next wesbite with Framer Motion and Tailwind CSS",
+      img: "/Metaverse.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", ],
+      link: "https://metaverse-gamma-pearl.vercel.app/",
+    },
+  
+    {
+      id: 2,
+      title: "",
+      des: " Modern UI/UX website, developed using React.js and Tailwind CSS, exemplifies modern UI/UX principles.",
+      img: "/Brainwave.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", ],
+      link: "https://brain-wave-demo.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://iphone-clone-omega.vercel.app/",
+    },
+  ];
+  
+  
+   
+   
+  
+ 
+  
+  export const workExperience = [
+   
+    {
+      id: 1,
+      title: "WooCommerce",
+      desc: "Designed and managed an e-commerce shop utilizing WooCommerce",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+   
+    {
+      id: 2,
+      title: "Freelance Developer",
+      desc: "Assisted in designing and adding additional pages, and led the revamp of a website .",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/JoeyKCS",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/joshua-mukwevho-67717b217/",
+    },
+  ];
